f4c869e9ffaf2c9f4528d6c83cb245bfeacf2d83
unknown <BT_4N2_09@BKTECH.LOCAL>
2022-09-23 12:27:24 -0400

    Initial commit

---
+++ src/U2L2Runner.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class U2L2Runner {
+    public static void main(String[] args) {
+        // I love repeating myself dry
+        Scanner userInput = new Scanner(System.in);
+        ConstructionPricer matPrices = new ConstructionPricer(11.50, 25.75, 0);
+    }
+}
